c432b44e1d4fefb330af6de56834118b7eb361b5
Jonathan Badger <jhbadger@gmail.com>
2021-11-21 19:36:15 -0500

    ML code

---
+++ All_vs_All_ML.R
@@ -0,0 +1,102 @@
+# ML all_vs_all on Summarized Experiment
+# J.H Badger (as part of McCulloch, et al. 2021)
+# November 9, 2021
+
+
+# Load needed libraries
+
+library(tidyverse)
+library(SummarizedExperiment)
+library(futile.logger)
+library(caret)
+library(MLeval)
+
+# summarized experiment object of data to analyze
+
+lkt <- readRDS("exp.rds")
+
+runML <- function(exp, response="Study_Clin_Response", 
+                  positive="Responder", method="rf", index=NULL,
+                  Group = "", seed=15, boot=1, metric=max) {
+  set.seed(seed)
+  train_control <- trainControl(method="cv", number=10, 
+                                classProbs=T, savePredictions = T)
+  if (is.null(index)) {
+    index <- createDataPartition(exp[[response]], p=0.70, list=FALSE)
+  }
+  models <- list()
+  accuracies <- c()
+  for(i in 1:boot) {
+    training <- exp[, as.vector(index)]
+    tData <- t(assay(training))
+    tResp <- factor(training[[response]])
+    models[[i]] <- train(tData, tResp, trControl = train_control, 
+                   method = method, preProcess=c("center", "scale"))
+    testing <- exp[,-as.vector(index)] %>% assay() %>% t()
+    obs <- factor(exp[,row.names(testing)][[response]])
+    predictions <- factor(predict(models[[i]], testing))
+    matrix <- confusionMatrix(predictions, obs,
+                  positive = positive)
+    accuracies[i] <- matrix$overall["Accuracy"]
+  }
+  accuracies <- round(accuracies, 3)
+  target_acc <- metric(accuracies)
+  model <- models[[which(accuracies==target_acc)[1]]]
+  if (is_null(model)) {
+    flog.error("null model detected")
+    model <- models[[which(!is.null(models))[1]]]
+  }
+  ppred <- data.frame(predict(model, testing, type="prob"))
+  ppred$obs <- obs
+  ppred$Group <- Group
+  roc <- evalm(ppred, silent=TRUE, plots="r", showplots = FALSE)
+  rstats <- roc$optres[[1]]
+  stats <- matrix$overall[1:6]
+  stats <- append(stats, rstats["AUC-ROC","Score"])
+  names(stats)[length(stats)] <- "AUC"
+  list(stats=stats,model=model)
+}
+
+testModel <- function(model, exp, seed=15, response="Study_Clin_Response",  positive="Responder", Group = "") {
+  set.seed(seed)
+  testing <- assay(exp) %>% t()
+  obs <- factor(exp[[response]])
+  predictions <- factor(predict(model, testing))
+  matrix <- confusionMatrix(predictions, obs,
+                    positive = positive)
+  ppred <- data.frame(predict(model, testing, type="prob"))
+  ppred$obs <- obs
+  ppred$Group <- Group
+  roc <- evalm(ppred, silent=TRUE, plots="r", showplots = FALSE)
+  rstats <- roc$optres[[1]]
+  stats <- matrix$overall[1:6]
+  stats <- append(stats, rstats["AUC-ROC","Score"])
+  names(stats)[length(stats)] <- "AUC"
+  stats
+}
+
+results <- NULL
+for(study in sort(unique(lkt$Study))) {
+  for(method in c("rf","svmPoly","glm")) {
+    flog.info(str_c(study," ", method))
+    mdl <- runML(lkt[,lkt$Study==study], method = method)
+    for(target in sort(unique(lkt$Study))) {
+      if (target==study) {
+        line <- append(c(Train=study, Test=target, Method=method), mdl$stats)
+      }
+      else {
+        line <- append(c(Train=study, Test=target, Method=method),
+                       testModel(mdl$model, 
+                                 lkt[,lkt$Study==target]))
+      }
+      if (is.null(results)) {
+        results <- t(data.frame(line))
+      } else {
+        results <- rbind(results, line)
+      }
+    }
+  }
+}
+
+write_tsv(data.frame(results),"results.tsv")
+
